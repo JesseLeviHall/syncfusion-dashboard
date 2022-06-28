62edfaa8f4b6bb0ee5011cab35d8be1999ac1983
jesse h <jesselevihall@gmail.com>
2022-06-28 15:00:35 -0600

    Sparkline Chart

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, width, height, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${yval}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+      xName="x"
+      yName="yval"
+      type={type}>
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+  return (
+    <ChartComponent width={width} height={height}>
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
