8e184da102bc5e35a373714d38cc6921ff750106
jesse h <jesselevihall@gmail.com>
2022-06-28 15:19:30 -0600

    Stacked Chart

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -9,6 +9,7 @@ import {
   StackingColumnSeries,
   Tooltip,
 } from "@syncfusion/ej2-react-charts";
+
 import {
   stackedCustomSeries,
   stackedPrimaryXAxis,
@@ -18,9 +19,24 @@ import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
   const { currentMode } = useStateContext();
+
   return (
-    <ChartComponent width={width} height={height}>
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}>
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
     </ChartComponent>
   );
 };
